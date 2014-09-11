d95a3322ed6eaa035e3200e1dc90b61d3aaf20e8
Julien Barnier <julien@nozav.org>
2014-09-11 11:44:36 +0200

    Initial import

---
+++ ddg2google.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name       ddg2google
+// @namespace  http://nozav.org/
+// @version    0.1// @description  Keyboard shortcut to send a duckduckgo query to google
+// @match      http://duckduckgo.com/*
+// @match      https://duckduckgo.com/*
+// @copyright  2014, Julien Barnier
+// ==/UserScript==
+
+function doc_keyUp(e) {
+    switch (e.keyCode) {
+        case 71:
+            //g
+            if (e.altKey) {
+        		var search_field = document.getElementById("search_form_input");
+                var search_string = escape(search_field.value)
+        		window.location = "https://encrypted.google.com/search?q="+search_string;
+            }
+        break;
+    }
+}
+
+document.addEventListener('keyup', doc_keyUp, false);
+
